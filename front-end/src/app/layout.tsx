5ae4e552f27dfc83b1587eae4ee5b7f056be0bcc
thu le <thule01265@gmail.com>
2024-09-28 20:43:12 +0700

    header customer

--- front-end/src/app/layout.tsx
+++ front-end/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
